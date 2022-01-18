cbf639dd388aba8459f1eb22e3b18d3b126c4173
L00phole <89979166+L00phole@users.noreply.github.com>
2022-01-18 16:25:23 +0100

    Deleted extra error page added a time-file

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 /*Import components */
-import Home from "./Pages/Home";
-import ErrorPage from "./Pages/ErrorPage";
+import Home from "./components/Pages/Home";
+import ErrorPage from "./components/Pages/ErrorPage";
 import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <Router>
-     
       <Navbar />
-  
+
       <Routes>
-     
         <Route path="/" element={<Home />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
-   
-    
     </Router>
-
-    
-  )
+  );
 }

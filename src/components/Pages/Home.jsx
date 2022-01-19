ae3809d3dc514a91c7a3418d46543fcadae7051f
GHHag <gustav.hagenblad@gmail.com>
2022-01-19 16:46:01 +0100

    Added form for adding city and time zone

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import AddCityForm from '../AddCityForm';
 
 const Home = () => {
   return (
     <div>
       <h1> Home</h1>
+
+      <AddCityForm />
+
     </div>
   )
 }

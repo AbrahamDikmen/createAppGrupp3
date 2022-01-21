751631146be997b53a3c31fa8ffe70d3522c369d
denjennie <denjennie@live.se>
2022-01-21 08:27:37 +0100

    Ändringar i navbaren

--- src/components/NavbarOne.jsx
+++ src/components/NavbarOne.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { Navbar } from 'react-bootstrap';
 import { Container } from 'react-bootstrap';
 import { Nav } from 'react-bootstrap';
@@ -7,7 +7,7 @@ import { Form } from 'react-bootstrap';
 import { FormControl } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 import '../Styles/Navbar.css'
-import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
+// import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
 
 
 const NavbarOne = () => {
@@ -18,20 +18,21 @@ const NavbarOne = () => {
     <Navbar.Brand id="Logo" href="#">TAJMING</Navbar.Brand>
     
     <Navbar.Toggle aria-controls="navbarScroll" />
-    <Navbar.Collapse id="navbarScroll">
-      <Nav
-        className="me-auto  my-lg-0"
-        style={{ maxHeight: '100px', }}
-            navbarScroll >
-               <Form className="d-flex">
+        <Navbar.Collapse id="navbarScroll">
+          
+      <Nav className="me-auto  my-lg-0 "
+            style={{ maxHeight: '100px', }}
+              navbarScroll >
+        <Form className="d-flex ">
         <FormControl id = "formControl"
-          type="search"
-          placeholder="Ange en stad"
-          className="me-2"
-          aria-label="Search"
+            type="search"
+            placeholder="Ange en stad"
+            className="me-2"
+            aria-label="Search"
         />
         <Button id="searchButton" variant="outline-success">Sök</Button>
-      </Form>
+            </Form>
+            
         <Nav.Link id="myCitys"href="#MinaStäder">Mina städer</Nav.Link>
       </Nav>
    

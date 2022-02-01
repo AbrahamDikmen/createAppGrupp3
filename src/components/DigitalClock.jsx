4683f72322e9d65576b68d6419ff03db6913f537
GHHag <gustav.hagenblad@gmail.com>
2022-02-01 20:25:48 +0100

    added loader to clock

--- src/components/DigitalClock.jsx
+++ src/components/DigitalClock.jsx
@@ -1,16 +1,18 @@
 import { useState, useEffect } from "react";
 import "../Styles/digital-clock.css";
+import Loader from '../components/Loader';
+
 export default function DigitalClock({ timezone }) {
 
-    //const [timeString, setTimeString] = useState(new Date().toLocaleTimeString());
-    const [timeString, setTimeString] = useState([]);
+    const [timeString, setTimeString] = useState("");
+    const [isLoading, setIsLoading] = useState(true);
 
     let specificCityTime = "";
 
     const getTime = async () => {
         let response = await fetch(
             `http://worldtimeapi.org/api/timezone/${timezone}`
-        );
+        )
 
         if (response.status === 200) {
             const time = await response.json();
@@ -23,21 +25,28 @@ export default function DigitalClock({ timezone }) {
 
     useEffect(() => {
         if (timezone) {
-            const timer = setInterval(() => {
-                getTime();
+            const timer = setInterval(async () => {
+                await getTime();
                 setTimeString(`${specificCityTime.substring(11, 19)}`);
+                setIsLoading(false);
             }, 1000);
-            return function cleanup() { clearInterval(timer); }
         }
         else {
+            setTimeString(new Date().toLocaleTimeString());
             const timer = setInterval(() => { setTimeString(new Date().toLocaleTimeString()) }, 1000);
-            return function cleanup() { clearInterval(timer); }
+            setIsLoading(false);
+        }
+        return function cleanup() {
+            clearInterval(timer);
         }
     }, []);
 
     return (
         <div className="digitalClock">
-            <h2>{timeString}</h2>
+            <h2>
+                {isLoading && <Loader />}
+                {timeString}
+            </h2>
         </div>
     );
 

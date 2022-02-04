fdc77d1420dde4967ad76c480ee456ec61590ac8
Abraham Dikmen <89247410+AbrahamDikmen@users.noreply.github.com>
2022-02-04 21:54:17 +0100

    Comments

--- src/components/GetPhoto.jsx
+++ src/components/GetPhoto.jsx
@@ -4,12 +4,15 @@ import { Container } from "react-bootstrap";
 const GetPhoto = ({ theCity }) => {
   const [imageLink, setImageLink] = useState("");
 
+  //A function to fetch the random-image-API and then return it
   async function getImage() {
     let imageCity = "";
     let response = await fetch(
       `https://pixabay.com/api/?key=25521224-067cb032904da2f120a0b95e2&q=${theCity}&image_type=photo`
     );
 
+    // The image that the APi returns depends entirely on what the user searched for
+    // It is searching for the second array to return and if there is none then it looks for the first array to return
     if (response.status === 200) {
       const imageJson = await response.json();
       imageCity =
